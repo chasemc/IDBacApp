cf97cbac688f1d564a9f3acb5e424f41039076e6
chasemc <cclark42@uic.edu>
2017-11-03 11:26:41 -0500

    .

--- R/runExample.R
+++
@@ -1,9 +0,0 @@
-#' @export
-runExample <- function() {
-  appDir <- "C:/Users/chase/Documents/R/win-library/3.4/IDBacApp/app"
-  if (appDir == "") {
-    stop("Could not find example directory. Try re-installing `mypackage`.", call. = FALSE)
-  }
-
-  shiny::runApp(appDir, display.mode = "normal")
-}
