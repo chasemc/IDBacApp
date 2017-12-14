8b80dce1fa7c2143497782d326cbd6496ac5f97d
chasemc <cclark42@uic.edu>
2017-12-14 15:02:48 -0600

    uodate with zenodo and layout

--- exe/ui.r
+++ exe/ui.r
@@ -34,16 +34,44 @@ Install_And_Load(Required_Packages)
 navbarPage(
   "IDBac",
   tabPanel(
+    "Introduction",
+    fluidPage(
+      fluidRow(
+        column(3,
+               p("To begin, select \"PreProcessing\" above:"),
+               
+               div(img(src="IDBac_Computer_SVG_300DPI.png",style="width:75%;height:75%"))),
+        column(8,
+               h3("Welcome to the IDBac application."),
+               br(),
+               h4("General Information:"),
+              tags$ul( 
+               tags$li(p("For more information about the method, as well as links to the full code, please visit ", a(href="https://chasemc.github.io/IDBac/", target="_blank", "chasemc.github.io/IDBac"))),
+               tags$li(p("Bugs and suggestions may be reported on the ", a(href="https://github.com/chasemc/IDBac_app/issues",target="_blank","IDBac Issues Page on GitHub.", img(border="0", title="https://github.com/chasemc/IDBac_app/issues", src="GitHub.png", width="25" ,height="25"))))),
+            
+              br(),
+              h4("How to Cite IDBac:"),
+              
+               p("If you use IDBac in your work please cite:") ,
+               p(strong("Reference"),
+                 p("Also cite this version of IDBac using the DOI:",tags$b("10.5281/zenodo.1115619")),
+                 p("Coupling MALDI-TOF mass spectrometry protein and specialized metabolite analyses to rapidly discriminate bacterial function
+                   Chase M. Clark, Maria S. Costa, Laura M. Sanchez, Brian T. Murphy
+                   bioRxiv 215350; doi: https://doi.org/10.1101/215350"),
+                 p("For past versions of IDBac can be found here:",a(href="https://doi.org/10.5281/zenodo.1115620",img(src="https://zenodo.org/badge/DOI/10.5281/zenodo.1115620.svg"),target="_blank") ),  
+                 a(href="https://doi.org/10.5281/zenodo.1115620",img(src="https://zenodo.org/badge/DOI/10.5281/zenodo.1115620.svg"),target="_blank")
+                 
+                 
+                 
+                 
+  ))))),
+  
+  
+  
+  tabPanel(
     "PreProcessing",
     fluidPage(
-       fluidRow(
-         column(3,div(img(src="IDBac_Computer_SVG_300DPI.png",style="width:75%;height:75%"))),
-        column(4,
-              h3("Welcome to the IDBac application."),
-              p("For more information, as well as links to the full code, please visit our ", a(href="https://chasemc.github.io/IDBac/", target="_blank", "website.")),
-              p("Bugs and suggestions may be reported on the ", a(href="https://github.com/chasemc/IDBac_app/issues",target="_blank","IDBac Issues Page on GitHub.", img(border="0", title="https://github.com/chasemc/IDBac_app/issues", src="GitHub.png", width="25" ,height="25"))),
-              p("If you use IDBac in your work please cite:") ,
-              p(strong("Reference"))),
+       
 
         column(1),
         column(4,
@@ -58,7 +86,7 @@ navbarPage(
 
             uiOutput("ui1")
 
-    ))),
+    )),
 
 
 
