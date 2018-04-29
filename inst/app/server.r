09e60fcdf296c0ae5679c313cc9adea8ed039103
chasemc <cclark42@uic.edu>
2018-04-29 09:52:55 -0500

    uncomment onSessionEnded

--- inst/app/server.r
+++ inst/app/server.r
@@ -2376,10 +2376,10 @@ asd123<<-pcaCalculation()
 
 
   #  The following code is necessary to stop the R backend when the user closes the browser window
-  # session$onSessionEnded(function() {
-  #    stopApp()
-  #    q("no")
-  #  })
+   session$onSessionEnded(function() {
+      stopApp()
+      q("no")
+    })
 
 
 
