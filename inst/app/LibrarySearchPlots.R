90814c680f6a1051afed373fc09e833461b7f089
Chase Clark <cclark42@uic.edu>
2018-06-20 10:58:13 -0500

    one to one is REALLY slow even run in parallel

--- inst/app/LibrarySearchPlots.R
+++ inst/app/LibrarySearchPlots.R
@@ -30,91 +30,123 @@ databaseSearch <- function(idbacPath, databasePath, wantReport){
   # Returns list of top hit matches. Each list element is one unknown sample, with one best cosine similarity score + lib sample ID
 
 
-  # This function processes library spectra, one at a time (to save RAM)
-  # read SQLite, bin and filter percent presence based on user input
-  # return single MALDIquant object of protein peaks
-  libraryProcessing <-  function(singleLibSpec){
-
-    # Return the "rds" SQL blob for the individual strain
-    libProteinPeaks <-  libSpec %>%
-      filter(Strain_ID == singleLibSpec) %>%
-      select(rds) %>%
-      collect()
-    #Decompress blob
-    libProteinPeaks <- memDecompress(libProteinPeaks[[1]][[1]], type="gzip")
-    # Unserialize blob
-    libProteinPeaks <- unserialize(libProteinPeaks, NULL)
-    # Unlist rds file list
-    libProteinPeaks <- unlist(libProteinPeaks, recursive = TRUE)
-    # Return only protein peak MALDIquant objects
-    libProteinPeaks <- libProteinPeaks[grep("ProteinPeaks", names(libProteinPeaks))]
-
-    # Get sample IDs contained in the metadata *within* the rds MALDIquant protein peaks object
-    labs <- sapply(libProteinPeaks, function(x)metaData(x)$Strain)
-    # Change to facter
-    labs <- factor(labs)
-    # Setup for-loop
-    new2 <- NULL
-    newPeaks <- NULL
-    # If libProteinPeaks contains more than one sample ID (it shouldn't), make sure to bin and
-    # filter peaks only within the sample sample
-    for (i in seq_along(levels(labs))) {
-      specSubset <- (which(labs == levels(labs)[[i]]))
-      if (length(specSubset) > 1) { # ie if there is > 1 protein spectrum (replicate)
-        libProteinPeaks <- MALDIquant::trim(libProteinPeaks, c(3000,15000))
-        # See here for info on "tolerance" value
-        # https://github.com/sgibb/MALDIquant/issues/56#issuecomment-388133351
-        libProteinPeaks <- MALDIquant::binPeaks(libProteinPeaks, tolerance = .002, method = "relaxed")
-        new <- filterPeaks(libProteinPeaks[specSubset], minFrequency= 0/100)
-        new <- mergeMassPeaks(new, method="mean")
-        new2 <- c(new2, new)
-      } else{ # If there is only one spectrum
-        new2 <- c(new2, libProteinPeaks[specSubset])
-      }
-
-    }
-    libProteinPeaks <- new2
-
-  }
-
-
-  # This function processes unknown spectra, one at a time (to save RAM)
-  # read rds protein peaks file (so we can bin against library spectrum later), bin and filter percent presence based on user input
-  # return single MALDIquant object of protein peaks
-  unknownProcessing <-  function(singleSpec){
-
-
-    unknownSpectrum <- unlist(strsplit(basename(unknownProteinPeakFiles), "_ProteinPeaks.rds"))
-
-    unknownSpectrum <- readRDS(unknownProteinPeakFiles[which(unknownSpectrum == singleSpec)])
-
-    labs <- sapply(unknownSpectrum, function(x)metaData(x)$Strain)
-    labs <- factor(labs)
-    new2 <- NULL
-    newPeaks <- NULL
-    for (i in seq_along(levels(labs))) {
-      specSubset <- (which(labs == levels(labs)[[i]]))
-      if (length(specSubset) > 1) {
-        unknownSpectrum <- MALDIquant::trim(unknownSpectrum, c(3000,15000))
-        unknownSpectrum <- MALDIquant::binPeaks(unknownSpectrum, tolerance = .002, method = "relaxed")
-        new <- filterPeaks(unknownSpectrum[specSubset],minFrequency= 0/100)
-        new<-mergeMassPeaks(new,method="mean")
-        new2 <- c(new2, new)
-      } else{
-        new2 <- c(new2, unknownSpectrum[specSubset])
-      }
-
-    }
-    unknownSpectrum <- new2
-
-  }
-
 
 
   # This is the workhorse of the searching.
   # Takes a list of unknown *Sample IDs* as input
 
-  searchLibrary<- function(unk){
+  searchLibrary<- function(unk, databasePathin){
+    .libPaths("C:/Users/chase/Documents/IDBacPackages")
+
+    library(dplyr)
+    library(RSQLite)
+    library(DBI)
+    library(MALDIquant)
+
+    # This function processes library spectra, one at a time (to save RAM)
+    # read SQLite, bin and filter percent presence based on user input
+    # return single MALDIquant object of protein peaks
+    libraryProcessing <-  function(singleLibSpec){
+
+      # Return the "rds" SQL blob for the individual strain
+      libProteinPeaks <-  libSpec %>%
+        filter(Strain_ID == singleLibSpec) %>%
+        select(rds) %>%
+        collect()
+      #Decompress blob
+      libProteinPeaks <- memDecompress(libProteinPeaks[[1]][[1]], type="gzip")
+      # Unserialize blob
+      libProteinPeaks <- unserialize(libProteinPeaks, NULL)
+      # Unlist rds file list
+      libProteinPeaks <- unlist(libProteinPeaks, recursive = TRUE)
+      # Return only protein peak MALDIquant objects
+      libProteinPeaks <- libProteinPeaks[grep("ProteinPeaks", names(libProteinPeaks))]
+
+      # Get sample IDs contained in the metadata *within* the rds MALDIquant protein peaks object
+      labs <- sapply(libProteinPeaks, function(x)metaData(x)$Strain)
+      # Change to facter
+      labs <- factor(labs)
+      # Setup for-loop
+      new2 <- NULL
+      newPeaks <- NULL
+      # If libProteinPeaks contains more than one sample ID (it shouldn't), make sure to bin and
+      # filter peaks only within the sample sample
+      for (i in seq_along(levels(labs))) {
+        specSubset <- (which(labs == levels(labs)[[i]]))
+        if (length(specSubset) > 1) { # ie if there is > 1 protein spectrum (replicate)
+          libProteinPeaks <- MALDIquant::trim(libProteinPeaks, c(3000,15000))
+          # See here for info on "tolerance" value
+          # https://github.com/sgibb/MALDIquant/issues/56#issuecomment-388133351
+          libProteinPeaks <- MALDIquant::binPeaks(libProteinPeaks, tolerance = .002, method = "relaxed")
+          new <- filterPeaks(libProteinPeaks[specSubset], minFrequency= 0/100)
+          new <- mergeMassPeaks(new, method="mean")
+          new2 <- c(new2, new)
+        } else{ # If there is only one spectrum
+          new2 <- c(new2, libProteinPeaks[specSubset])
+        }
+
+      }
+      libProteinPeaks <- new2
+
+    }
+
+
+    # This function processes unknown spectra, one at a time (to save RAM)
+    # read rds protein peaks file (so we can bin against library spectrum later), bin and filter percent presence based on user input
+    # return single MALDIquant object of protein peaks
+    unknownProcessing <-  function(singleSpec){
+
+
+      unknownSpectrum <- unlist(strsplit(basename(unknownProteinPeakFiles), "_ProteinPeaks.rds"))
+
+      unknownSpectrum <- readRDS(unknownProteinPeakFiles[which(unknownSpectrum == singleSpec)])
+
+      labs <- sapply(unknownSpectrum, function(x)metaData(x)$Strain)
+      labs <- factor(labs)
+      new2 <- NULL
+      newPeaks <- NULL
+      for (i in seq_along(levels(labs))) {
+        specSubset <- (which(labs == levels(labs)[[i]]))
+        if (length(specSubset) > 1) {
+          unknownSpectrum <- MALDIquant::trim(unknownSpectrum, c(3000,15000))
+          unknownSpectrum <- MALDIquant::binPeaks(unknownSpectrum, tolerance = .002, method = "relaxed")
+          new <- filterPeaks(unknownSpectrum[specSubset],minFrequency= 0/100)
+          new<-mergeMassPeaks(new,method="mean")
+          new2 <- c(new2, new)
+        } else{
+          new2 <- c(new2, unknownSpectrum[specSubset])
+        }
+
+      }
+      unknownSpectrum <- new2
+
+    }
+
+
+
+    # connect to user-specified database
+    dbcon <- DBI::dbConnect(RSQLite::SQLite(), databasePathin)
+
+    # Connect dplyr to database
+    db <- dplyr::tbl(dbcon, "IDBacDatabase")
+
+    # Filter database by whatever metadata
+    # Return only rds column and strain ID
+    libSpec <- db %>%
+      #filter(Strain_ID == "114A-2") %>%
+      dplyr::select(c(Strain_ID,rds))
+
+
+
+    # Note: cannot "slice()" or select by index within SQL table, so will just
+    # filter by strain ID
+    libStrainIDs <- libSpec %>%
+      select(Strain_ID) %>%
+      collect() %>%
+      unlist() %>%
+      as.vector()
+
+
 
     # Use Cosine Similarity to determine spectra similarity (Note: 0 is close, 1 is not close)
     cosineD <- function(x) {
@@ -145,44 +177,48 @@ databaseSearch <- function(idbacPath, databasePath, wantReport){
 
   }
 
+
+
+
+
   # Get "Unknown" strain protein peak files
   unknownProteinPeakFiles <- list.files(paste0(idbacPath, "\\Peak_Lists"),full.names = TRUE, pattern = "ProteinPeaks.rds")
-
-
-  # connect to user-specified database
-  dbcon <- DBI::dbConnect(RSQLite::SQLite(), databasePath)
-
-  # Connect dplyr to database
-  db <- dplyr::tbl(dbcon, "IDBacDatabase")
-
-  # Filter database by whatever metadata
-  # Return only rds column and strain ID
-  libSpec <- db %>%
-    #filter(Strain_ID == "114A-2") %>%
-    dplyr::select(c(Strain_ID,rds))
-
-  # Finds the number of rows in the currently-loaded databsase
-  lengthLibSpecs <- libSpec %>%
-    dplyr::summarize(n()) %>%
-    dplyr::collect() %>%
-    as.numeric()
-
-
 
   unknownStrainIDs <- unlist(strsplit(basename(unknownProteinPeakFiles), "_ProteinPeaks.rds"))
 
 
-  # Note: cannot "slice()" or select by index within SQL table, so will just
-  # filter by strain ID
-  libStrainIDs <- libSpec %>%
-    select(Strain_ID) %>%
-    collect() %>%
-    unlist() %>%
-    as.vector()
+
+
+
+
+
+
 
   # Run the library search. Inputs: "unknownStrainIDs" ; "searchLibrary" = function
   # Returns list of top hit matches. Each list element = 1 unknown sample, with 1 best cosine similarity score = lib sample ID
-  allScores <- lapply(unknownStrainIDs, searchLibrary)
+  #allScores <- lapply(unknownStrainIDs, searchLibrary, databasePathin = databasePath)
+
+
+
+        try(numCores <- parallel::detectCores())
+
+        if(exists("numCores") & numCores > 2){
+        numCores <- parallel::makeCluster(numCores-1)
+
+        allScores <- parallel::parLapply(numCores, unknownStrainIDs, searchLibrary, databasePathin = databasePath)
+
+        parallel::stopCluster(numCores)
+        }
+
+
+
+
+
+
+
+
+
+
 
   allScores <- do.call(rbind, allScores)
   allScores <- cbind(unknown = unknownStrainIDs, allScores)
