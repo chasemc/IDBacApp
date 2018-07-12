f9a9fee355a9c94a123f9d0ea78322a623e1e003
Chase Clark <cclark42@uic.edu>
2018-07-12 10:19:22 -0500

    Explicit namespace and CHANGE "mean" to "sum" when merge peaks
      # If we use "mean", peaks that show up often can be weighted less than a peak that shows up once with high intensity, so now using "sum"

--- R/collapseProteinReplicates.r
+++ R/collapseProteinReplicates.r
@@ -1,17 +1,17 @@
 
-collpaseProteinReplicates <- function(trimmedProteinPeakList, proteinPercentPresence){
+collapseProteinReplicates <- function(trimmedProteinPeakList, proteinPercentPresence){
 
-
-
-labs <- sapply(trimmedProteinPeakList, function(x)metaData(x)$Strain)
+labs <- sapply(trimmedProteinPeakList, function(x)  MALDIquant::metaData(x)$Strain)
 labs <- factor(labs)
 new2 <- NULL
 newPeaks <- NULL
 for (i in seq_along(levels(labs))) {
   specSubset <- (which(labs == levels(labs)[[i]]))
   if (length(specSubset) > 1) {
-    new <- filterPeaks(trimmedProteinPeakList[specSubset], minFrequency = proteinPercentPresence / 100)
-    new <- mergeMassPeaks(new, method="mean")
+    new <- MALDIquant::filterPeaks(trimmedProteinPeakList[specSubset], minFrequency = proteinPercentPresence / 100)
+    new <- MALDIquant::mergeMassPeaks(new, method="sum")
+  # If we use "mean", peaks that show up often can be weighted less than a peak that shows up once with high intensity, so now using "sum"
+
     new2 <- c(new2, new)
   } else{
     new2 <- c(new2, trimmedProteinPeakList[specSubset])
