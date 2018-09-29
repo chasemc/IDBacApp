6bb8f89d25164a61b9ed2cb788c4d65df40525f4
chasemc <cclark42@uic.edu>
2018-09-29 15:44:19 -0500

    change from dplyr to SQL

--- R/sqlManipulators.r
+++ R/sqlManipulators.r
@@ -9,254 +9,121 @@
 # where rds was before
 
 
-#need to get and collapse small and protein in diff functions b/c binpeak tolerance
 
-getProteinPeakData <-  function(db, filesha1){
+getProteinPeakData <-  function(db, fileshas){
 
-  # "db" = dplyr connection to SQLite DB
+  sqlQ <- glue::glue_sql("
+                          SELECT `proteinPeaks`
+                          FROM (SELECT *
+                                  FROM `IndividualSpectra`
+                                WHERE (`filesha1` IN ({airports*})))
+                          WHERE (`proteinPeaks` IS NOT NULL)",
+                                                   airports = fileshas,
+                                                   .con = db
+  )
 
-              var <- enquo(filesha1)
-              db %>%
-                filter(filesha1 %in% rlang::eval_tidy(var)) %>%
-                filter(proteinPeaks != "NA") %>%
-                select(proteinPeaks) %>%
-                collect() %>%
-              return(.) -> p
+  conn <- pool::poolCheckout(db)
+  airport <- DBI::dbSendQuery(conn, sqlQ)
 
-              p <- unname(unlist(p, recursive = FALSE))
-
-            unlist(lapply(p, function(x) unserialize(memDecompress(x, type= "gzip")))  )
-}
-
-
-collapseProteinReplicates <- function(db, filesha1, proteinPercentPresence){
-                                IDBacApp::getProteinPeakData(db, filesha1) %>%
-                                MALDIquant::binPeaks(., tolerance = .002) %>%
-                                MALDIquant::filterPeaks(.,
-                                                          minFrequency = proteinPercentPresence / 100) %>%
-
-                                  MALDIquant::mergeMassPeaks()
-}
-
-
-
-
-
-proteiny <- function(fileshas,
-                     db,
-                     proteinPercentPresence,
-                     lowerMassCutoff,
-                     upperMassCutoff){
-
-
-
-          # get filesha1 and strain ids
-          db %>%
-            filter(filesha1 %in% fileshas) %>%
-            pull(filesha1) %>%
-                        IDBacApp::collapseProteinReplicates(db = db,
-                                                filesha1 = .,
-                                                proteinPercentPresence = proteinPercentPresence) %>%
-            MALDIquant::trim(., c(lowerMassCutoff,
-                                  upperMassCutoff))
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#---------------------------
-#---------------------------
-
-
-
-
-
-
-
-
-getSmallMolPeakData <-  function(db, filesha1){
-
-  # "db" = dplyr connection to SQLite DB
-
-  var <- enquo(filesha1)
-  db %>%
-    filter(filesha1 %in% rlang::eval_tidy(var)) %>%
-    filter(smallMoleculePeaks != "NA") %>%
-    select(smallMoleculePeaks) %>%
-    collect() %>%
-    return(.) -> p
+  p <- DBI::dbFetch(airport)
+  pool::poolReturn(conn)
 
   p <- unname(unlist(p, recursive = FALSE))
-
   unlist(lapply(p, function(x) unserialize(memDecompress(x, type= "gzip")))  )
 }
 
 
-collapseSmallMolReplicates <- function(db, filesha1, smallMolPercentPresence){
-  IDBacApp::getSmallMolPeakData(db, filesha1) %>%
-    MALDIquant::binPeaks(., tolerance = .002) %>%
+collapseProteinReplicates <- function(db,
+                                      fileshas,
+                                      proteinPercentPresence,
+                                      lowerMassCutoff,
+                                      upperMassCutoff){
+
+                                IDBacApp::getProteinPeakData(db,
+                                                             fileshas) %>%
+                                MALDIquant::binPeaks(.,
+                                                     tolerance = .02) %>%
+                                MALDIquant::filterPeaks(.,
+                                                          minFrequency = proteinPercentPresence / 100) %>%
+                                MALDIquant::mergeMassPeaks() %>%
+                                MALDIquant::trim(.,
+                                                 c(lowerMassCutoff,
+                                                   upperMassCutoff))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#---------------------------
+#---------------------------
+
+
+
+
+
+
+
+
+getSmallMolPeakData <-   function(db, fileshas){
+
+  sqlQ <- glue::glue_sql("
+                          SELECT `proteinPeaks`
+                          FROM (SELECT *
+                                  FROM `IndividualSpectra`
+                                WHERE (`filesha1` IN ({airports*})))
+                          WHERE (`proteinPeaks` IS NOT NULL)",
+                         airports = fileshas,
+                         .con = db
+  )
+
+  conn <- pool::poolCheckout(db)
+  airport <- DBI::dbSendQuery(conn, sqlQ)
+
+  p <- DBI::dbFetch(airport)
+  pool::poolReturn(conn)
+
+  p <- unname(unlist(p, recursive = FALSE))
+  unlist(lapply(p, function(x) unserialize(memDecompress(x, type= "gzip")))  )
+
+}
+
+
+collapseSmallMolReplicates <- function(db,
+                                       fileshas,
+                                       smallMolPercentPresence,
+                                       lowerMassCutoff,
+                                       upperMassCutoff){
+
+  IDBacApp::getSmallMolPeakData(db,
+                               fileshas) %>%
+    MALDIquant::binPeaks(.,
+                         tolerance = .02) %>%
     MALDIquant::filterPeaks(.,
                             minFrequency = smallMolPercentPresence / 100) %>%
+    MALDIquant::mergeMassPeaks() %>%
+    MALDIquant::trim(.,
+                     c(lowerMassCutoff,
+                       upperMassCutoff))
 
-    MALDIquant::mergeMassPeaks()
+
 }
-
-
-
-
-smallMoly <- function(fileshas,
-                     db,
-                     smallMolPercentPresence,
-                     lowerMassCutoff,
-                     upperMassCutoff){
-
-
-
-  # get filesha1 and strain ids
-  db %>%
-    filter(filesha1 %in% fileshas) %>%
-    filter(smallMoleculePeaks != "NA") %>%
-    pull(filesha1) %>%
-    IDBacApp::collapseSmallMolReplicates(db = db,
-                                        filesha1 = .,
-                                        smallMolPercentPresence = smallMolPercentPresence) %>%
-    MALDIquant::trim(., c(lowerMassCutoff,
-                          upperMassCutoff))
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
