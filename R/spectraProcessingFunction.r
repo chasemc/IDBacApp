ceab364a3f171ed740751c0ea3d82b232615d663
chasemc <cclark42@uic.edu>
2018-12-31 18:33:02 -0600

    Prevent adding duplicate spectra

--- R/spectraProcessingFunction.r
+++ R/spectraProcessingFunction.r
@@ -27,153 +27,172 @@ spectraProcessingFunction <- function(rawDataFilePath,
   mzML_con <- mzR::openMSfile(rawDataFilePath,
                               backend = "pwiz")
   
- XMLinfo <- IDBacApp::createXMLSQL(sampleID = sampleID,
-                         rawDataFilePath = rawDataFilePath,
-                         userDBCon = userDBCon,
-                         mzML_con = mzML_con)
+  XMLinfo <- IDBacApp::createXMLSQL(sampleID = sampleID,
+                                    rawDataFilePath = rawDataFilePath,
+                                    userDBCon = userDBCon,
+                                    mzML_con = mzML_con)
   
   
   # Get number of spectra contained in mzML
   scanNumber <- nrow(mzR::header(mzML_con))
   
-  # Lapply over each spectrum inside an mzmL file
-  wq <- lapply(1:scanNumber, 
-               function(individualSpectrum){
-                 
-                 sqlDataFrame <- IDBacApp::sqlTableArchitecture(nrow = 1)
-                 
-                 sqlDataFrame$IndividualSpectra$mzMLSHA <- XMLinfo
-                 sqlDataFrame$IndividualSpectra$Strain_ID <- sampleID
-                 sqlDataFrame$IndividualSpectra$spectrumSHA <- individualRawSpecSHA$spectrumSHA[[individualSpectrum]]
-                 sqlDataFrame$IndividualSpectra$MassError <- acquisitonInfo$MassError[[individualSpectrum]]
-                 sqlDataFrame$IndividualSpectra$AcquisitionDate <- acquisitonInfo$AcquisitionDate[[individualSpectrum]]
-                 
-                 
-                 
-                 
-                 spectraImport <- mzR::peaks(mzML_con, scans = individualSpectrum)
-                 
-                 if(typeof(spectraImport) == "list"){
-                   
-                   spectraImport <- lapply(spectraImport, function(x) MALDIquant::createMassSpectrum(mass = x[ , 1],
-                                                                                                     intensity = x[ , 2],
-                                                                                                     metaData = list(File = acquisitonInfo$rawFilePaths[[individualSpectrum]],
-                                                                                                                     Strain = ids)))
-                 } else if(typeof(spectraImport) == "double") {
-                   
-                   spectraImport <- MALDIquant::createMassSpectrum(mass = spectraImport[ , 1],
-                                                                   intensity = spectraImport[ , 2],
-                                                                   metaData = list(File = acquisitonInfo$rawFilePaths[[individualSpectrum]],
-                                                                                   Strain = ids))
-                 }
-                 
-                 
-                 # Make sure to return spectraImport as a MassSpectrumList
-                 if(MALDIquant::isMassSpectrumList(spectraImport)){
-                   
-                 }else if(MALDIquant::isMassSpectrum(spectraImport)){
-                   spectraImport <- list(spectraImport)
-                 }
-                 
-                 
-                 
-                 
-                 if(max(MALDIquant::mass(spectraImport[[1]])) > 5000){ # if it's a protein spectrum
-                   
-                   sqlDataFrame$IndividualSpectra$proteinSpectrum <- IDBacApp::compress(IDBacApp::serial(spectraImport))
-                   
-                   
-                   
-                   # Why square root transformation and not log:
-                   #  Anal Bioanal Chem. 2011 Jul; 401(1): 167–181.
-                   # Published online 2011 Apr 12. doi:  10.1007/s00216-011-4929-z
-                   #"Especially for multivariate treatment of MALDI imaging data, the square root transformation can be considered for the data preparation
-                   #because for all three intensity groups in Table 1, the variance is approximately constant."
-                   
-                   
-                   spectraImport <- MALDIquant::transformIntensity(spectraImport, 
-                                                                   method = "sqrt") 
-                   spectraImport <- MALDIquant::smoothIntensity(spectraImport,
-                                                                method = "SavitzkyGolay", 
-                                                                halfWindowSize = 20) 
-                   spectraImport <- MALDIquant::removeBaseline(spectraImport,
-                                                               method = "TopHat") 
-                   spectraImport <- MALDIquant::calibrateIntensity(spectraImport,
-                                                                   method="TIC")  
-                   spectraImport <- MALDIquant::detectPeaks(spectraImport, 
-                                                            method = "MAD", 
-                                                            halfWindowSize = 20, 
-                                                            SNR = 3)
-                   spectraImport <- IDBacApp::serial(spectraImport)
-                   spectraImport <- IDBacApp::compress(spectraImport)
-                   sqlDataFrame$IndividualSpectra$proteinPeaks <- list(spectraImport)
-                   
-                   
-                 }else{
-                   ############
-                   #Spectra Preprocessing, Peak Picking
-                   sqlDataFrame$IndividualSpectra$smallMoleculeSpectrum <- IDBacApp::serial(spectraImport) 
-                   sqlDataFrame$IndividualSpectra$smallMoleculeSpectrum <- IDBacApp::compress(sqlDataFrame$IndividualSpectra$smallMoleculeSpectrum)
-                   sqlDataFrame$IndividualSpectra$smallMoleculeSpectrum <- list(sqlDataFrame$IndividualSpectra$smallMoleculeSpectrum)
-                   
-                   
-                   
-                   spectraImport <- MALDIquant::smoothIntensity(spectraImport,
-                                                                method = "SavitzkyGolay",
-                                                                halfWindowSize = 20) 
-                   spectraImport <- MALDIquant::removeBaseline(spectraImport,
-                                                               method = "TopHat")
-                   spectraImport <- MALDIquant::calibrateIntensity(spectraImport, 
-                                                                   method="TIC") 
-                   #Find all peaks with SNR >1, this will allow us to filter by SNR later, doesn't effect the peak-picking algorithm, just makes files bigger
-                   spectraImport <- MALDIquant::detectPeaks(spectraImport, 
-                                                            method = "SuperSmoother",
-                                                            halfWindowSize = 20, 
-                                                            SNR = 1)
-                   spectraImport <- IDBacApp::serial(spectraImport)
-                   spectraImport <- IDBacApp::compress(spectraImport)
-                   sqlDataFrame$IndividualSpectra$smallMoleculePeaks <- list(spectraImport)
-                   
-                 }
-                 a <- colnames(IDBacApp::sqlTableArchitecture(nrow = 1)$IndividualSpectra)
-                 b <- colnames(sqlDataFrame$IndividualSpectra)
-                 
-                 for(i in a[!a %in% b]){
-                   sqlDataFrame$IndividualSpectra[,i] <- NA
-                 }
-                 
-                 
-                 return(sqlDataFrame$IndividualSpectra[1, a])
-                 
-                 
-                 
-               })
+  # Loop over each spectrum inside an mzmL file
   
-  
-  
-  
-  wqq <- unlist(wq, recursive = FALSE)
-  
-  
-  for(i in base::seq_along(wqq)){
-    
-    # Write to SQL DB
-    DBI::dbWriteTable(conn = conn,
-                      name = "IndividualSpectra", # SQLite table to insert into
-                      wqq[[i]], # Insert single row into DB
-                      append = TRUE, # Append to existing table
-                      overwrite = FALSE) # Do not overwrite
-  }
-  pool::poolReturn(conn)
-  
-  
-  
+  IDBacApp::createSpectraSQL(mzML_con = mzML_con,
+                             scanNumber = scanNumber,
+                             userDBCon = userDBCon,
+                             sampleID = sampleID,
+                             XMLinfo = XMLinfo)
 }
 
 
+createSpectraSQL <- function(mzML_con, 
+                             scanNumber,
+                             userDBCon,
+                             sampleID,
+                             XMLinfo){
+  
+  for (individualSpectrum in 1:scanNumber){
+    
+    sqlDataFrame <- IDBacApp::sqlTableArchitecture(numberScans = 1)
+   
+    spectraImport <- mzR::peaks(mzML_con, 
+                                scans = individualSpectrum)
+    sqlDataFrame$IndividualSpectra$spectrumSHA <- IDBacApp::createMZsha(spectraImport)
+    
+     
+    if("IndividualSpectra" %in% DBI::dbListTables(userDBCon)){
+      
+      existingSHA <- glue::glue_sql("SELECT `spectrumSHA`
+                                    FROM `IndividualSpectra`",
+                                    .con = userDBCon)
+      
+      existingSHA <- DBI::dbGetQuery(conn = userDBCon,
+                                     statement = existingSHA)
+      
+    } else {
+      existingSHA <- matrix(NA)
+    }
+    
+    if(sqlDataFrame$IndividualSpectra$spectrumSHA %in% existingSHA[,1]) {
+      warning ("One of the spectra for \"", sampleID, "\" already seems to be present, spectrum not added again.")
+    } else {
+    
+    
+    sqlDataFrame$IndividualSpectra$mzMLSHA <- XMLinfo$mzMLSHA
+    sqlDataFrame$IndividualSpectra$Strain_ID <- sampleID
+    
+    if("MassError" %in% ls(XMLinfo$mzMLInfo)){
+      sqlDataFrame$IndividualSpectra$MassError <- acquisitonInfo$MassError[[individualSpectrum]]
+    }
+    sqlDataFrame$IndividualSpectra$AcquisitionDate <- XMLinfo$mzMLInfo$AcquisitionDate
+    
+    
+    
+    
+    if(typeof(spectraImport) == "list"){
+      
+      spectraImport <- lapply(spectraImport, function(x) MALDIquant::createMassSpectrum(mass = x[ , 1],
+                                                                                        intensity = x[ , 2],
+                                                                                        metaData = list(File = rawDataFilePath,
+                                                                                                        Strain = sampleID)))
+    } else if(typeof(spectraImport) == "double") {
+      
+      spectraImport <- MALDIquant::createMassSpectrum(mass = spectraImport[ , 1],
+                                                      intensity = spectraImport[ , 2],
+                                                      metaData = list(File = rawDataFilePath,
+                                                                      Strain = sampleID))
+    }
+    
+    
+    # Make sure to return spectraImport as a MassSpectrumList
+    if(MALDIquant::isMassSpectrumList(spectraImport)){
+      
+    }else if(MALDIquant::isMassSpectrum(spectraImport)){
+      spectraImport <- list(spectraImport)
+    }
+    
+    
+    
+    
+    if(max(MALDIquant::mass(spectraImport[[1]])) > 5000){ # if it's a protein spectrum
+      
+      sqlDataFrame$IndividualSpectra$proteinSpectrum <- list(IDBacApp::compress(IDBacApp::serial(spectraImport)))
+      
+      
+      
+      # Why square root transformation and not log:
+      #  Anal Bioanal Chem. 2011 Jul; 401(1): 167–181.
+      # Published online 2011 Apr 12. doi:  10.1007/s00216-011-4929-z
+      #"Especially for multivariate treatment of MALDI imaging data, the square root transformation can be considered for the data preparation
+      #because for all three intensity groups in Table 1, the variance is approximately constant."
+      
+      
+      spectraImport <- MALDIquant::transformIntensity(spectraImport, 
+                                                      method = "sqrt") 
+      spectraImport <- MALDIquant::smoothIntensity(spectraImport,
+                                                   method = "SavitzkyGolay", 
+                                                   halfWindowSize = 20) 
+      spectraImport <- MALDIquant::removeBaseline(spectraImport,
+                                                  method = "TopHat") 
+      spectraImport <- MALDIquant::calibrateIntensity(spectraImport,
+                                                      method="TIC")  
+      spectraImport <- MALDIquant::detectPeaks(spectraImport, 
+                                               method = "MAD", 
+                                               halfWindowSize = 20, 
+                                               SNR = 3)
+      spectraImport <- IDBacApp::serial(spectraImport)
+      spectraImport <- IDBacApp::compress(spectraImport)
+      sqlDataFrame$IndividualSpectra$proteinPeaks <- list(spectraImport)
+      
+      
+    }else{
+      ############
+      #Spectra Preprocessing, Peak Picking
+      
+      sqlDataFrame$IndividualSpectra$smallMoleculeSpectrum <- list(IDBacApp::compress(IDBacApp::serial(spectraImport)))
+      
+      
+      spectraImport <- MALDIquant::smoothIntensity(spectraImport,
+                                                   method = "SavitzkyGolay",
+                                                   halfWindowSize = 20) 
+      spectraImport <- MALDIquant::removeBaseline(spectraImport,
+                                                  method = "TopHat")
+      spectraImport <- MALDIquant::calibrateIntensity(spectraImport, 
+                                                      method="TIC") 
+      #Find all peaks with SNR >1, this will allow us to filter by SNR later, doesn't effect the peak-picking algorithm, just makes files bigger
+      spectraImport <- MALDIquant::detectPeaks(spectraImport, 
+                                               method = "SuperSmoother",
+                                               halfWindowSize = 20, 
+                                               SNR = 1)
+      spectraImport <- IDBacApp::serial(spectraImport)
+      spectraImport <- IDBacApp::compress(spectraImport)
+      sqlDataFrame$IndividualSpectra$smallMoleculePeaks <- list(spectraImport)
+      
+    }
+    a <- colnames(IDBacApp::sqlTableArchitecture(numberScans = 1)$IndividualSpectra)
+    b <- colnames(sqlDataFrame$IndividualSpectra)
+    
+    for(i in a[!a %in% b]){
+      sqlDataFrame$IndividualSpectra[,i] <- NA
+    }
+    
+    
+    
+    
+    
+    
+    
+    # Write to SQL DB
+    DBI::dbWriteTable(conn = userDBCon,
+                      name = "IndividualSpectra", # SQLite table to insert into
+                      sqlDataFrame$IndividualSpectra[1, a], # Insert single row into DB
+                      append = TRUE, # Append to existing table
+                      overwrite = FALSE) # Do not overwrite
+  }
+}
 
-
-
+}
 
 
 
@@ -259,33 +278,33 @@ createXMLSQL <- function(rawDataFilePath,
   }
   
   
-    # Generate base SQL table
-    
-    #------- Create SQL "XML" table entry
-    
-    
-    # Get instrument Info
-    instInfo <- mzR::instrumentInfo(mzML_con)
-    instInfo <- as.data.frame(instInfo, 
-                              stringsAsFactors = F)
-    
-    sqlDataFrame$XML$manufacturer  <- instInfo$manufacturer
-    sqlDataFrame$XML$model         <- instInfo$model
-    sqlDataFrame$XML$ionisation    <- instInfo$ionisation
-    sqlDataFrame$XML$analyzer      <- instInfo$analyzer
-    sqlDataFrame$XML$detector      <- instInfo$detector
-    
-    # Find acquisitonInfo from mzML file
-    acquisitonInfo <- IDBacApp::findAcquisitionInfo(rawDataFilePath,
-                                                    instInfo$manufacturer)
-    
-    if("Instrument_MetaFile" %in% ls(acquisitonInfo)){ 
-      sqlDataFrame$XML$Instrument_MetaFile <- IDBacApp::serial(acquisitonInfo$Instrument_MetaFile)
-    }
-    
-    if (sqlDataFrame$XML$mzMLSHA %in% existingSHA[,1]) {
-      warning ("A mzML file matching \"", sampleID, "\" already seems to be present, file and contents not added again.")
-    } else {
+  # Generate base SQL table
+  
+  #------- Create SQL "XML" table entry
+  
+  
+  # Get instrument Info
+  instInfo <- mzR::instrumentInfo(mzML_con)
+  instInfo <- as.data.frame(instInfo, 
+                            stringsAsFactors = F)
+  
+  sqlDataFrame$XML$manufacturer  <- instInfo$manufacturer
+  sqlDataFrame$XML$model         <- instInfo$model
+  sqlDataFrame$XML$ionisation    <- instInfo$ionisation
+  sqlDataFrame$XML$analyzer      <- instInfo$analyzer
+  sqlDataFrame$XML$detector      <- instInfo$detector
+  
+  # Find acquisitonInfo from mzML file
+  acquisitonInfo <- IDBacApp::findAcquisitionInfo(rawDataFilePath,
+                                                  instInfo$manufacturer)
+  
+  if("Instrument_MetaFile" %in% ls(acquisitonInfo)){ 
+    sqlDataFrame$XML$Instrument_MetaFile <- IDBacApp::serial(acquisitonInfo$Instrument_MetaFile)
+  }
+  
+  if (sqlDataFrame$XML$mzMLSHA %in% existingSHA[,1]) {
+    warning ("A mzML file matching \"", sampleID, "\" already seems to be present, file and contents not added again.")
+  } else {
     # Write to SQL DB
     DBI::dbWriteTable(conn = userDBCon,
                       name = "XML", # SQLite table to insert into
@@ -295,7 +314,7 @@ createXMLSQL <- function(rawDataFilePath,
   }
   
   QQ<<-(list(mzMLSHA = sqlDataFrame$XML$mzMLSHA,
-              mzMLInfo = acquisitonInfo))
+             mzMLInfo = acquisitonInfo))
 }
 
 
@@ -326,7 +345,7 @@ findAcquisitionInfo <- function(rawFilepaths,
       }
     } else {
       sha$AcquisitionDate <-  base::Sys.Date() 
-
+      
     }
     
   })
