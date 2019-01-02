19476cfb47106c7ac7bcfc1c7169502c18ba9fb8
chasemc <cclark42@uic.edu>
2019-01-02 11:02:36 -0600

    msconvert finder

---
+++ R/findMSconvert.R
@@ -0,0 +1,56 @@
+#' @title Find msconvert.exe
+#' @export
+#' @rdname findMSconvert
+#' @param proteoWizardLocation optional filepath of msconvert.exe to be checked for validity
+#' @return filepath of the folder containing msconvert.exe
+
+findMSconvert <- function(proteoWizardLocation = character()){
+  
+  # Msconvert only works on Windows so abort function if not on Windows
+  os <- IDBacApp::getOS()
+  if (os == "windows") {
+    
+    # Look for msconvert if a path wasn't provided
+    if(length(proteoWizardLocation) == 0) {
+      
+      # Check 64
+      proteoWizardLocation <- base::shell(cmd = "ECHO %ProgramFiles%\\ProteoWizard", 
+                                          translate = TRUE, 
+                                          intern = T)
+      
+      proteoWizardLocation <- base::list.files(proteoWizardLocation,
+                                               recursive = TRUE, 
+                                               pattern = "msconvert.exe",
+                                               full.names = TRUE)
+      
+      if(length(proteoWizardLocation) == 0) {
+        # Check 32
+        proteoWizardLocation2 <- base::shell(cmd = "ECHO %programfiles(x86)%\\ProteoWizard", 
+                                             translate = TRUE, 
+                                             intern = T)
+        
+        proteoWizardLocation <- base::list.files(proteoWizardLocation,
+                                                 recursive = TRUE, 
+                                                 pattern = "msconvert.exe",
+                                                 full.names = TRUE)
+      }
+    }
+    
+    foundMSconvert <- tryCatch(base::file.exists(proteoWizardLocation),
+                               error = function(e) return(FALSE))[[1]]
+    
+  if(foundMSconvert){
+ 
+    proteoWizardLocation <- base::normalizePath(proteoWizardLocation[[1]])
+    
+  } else {
+    proteoWizardLocation <- "error"
+     warning("Unable to find msconvert.exe")
+    
+  }
+}
+
+
+return(proteoWizardLocation)
+
+}
