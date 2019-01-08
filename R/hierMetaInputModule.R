772e9a3bcec630bf7bd5bbd5f7a3300650ce3bbe
chasemc <cclark42@uic.edu>
2019-01-08 15:18:07 -0600

    Update hierMetaInputModule.R

--- R/hierMetaInputModule.R
+++ R/hierMetaInputModule.R
@@ -6,37 +6,32 @@ runDendDots <- function(rawDendrogram, trimdLabsDend, pool, columnID) {
   dendLabs <- labels(rawDendrogram)
   
   
-  if(!is.null(columnID)) {
-    
-    columnID <- columnID
-    
-    
     query <- DBI::dbSendStatement("SELECT *
                                   FROM metaData
                                   WHERE `Strain_ID` = ?",
                                   con=conn)
+    
     DBI::dbBind(query, list(dendLabs))
     selectedMeta <- DBI::dbFetch(query)
+    
     dbClearResult(query)
     
     
-    selectedMeta <- base::cbind.data.frame(ids = selectedMeta$Strain_ID,
-                                           meta = selectedMeta[,colnames(selectedMeta) %in% columnID])
+    selectedMeta <- selectedMeta[ ,colnames(selectedMeta) %in% columnID]
+    uniq <- unique(selectedMeta)
+    selectedMeta <- sapply(uniq, function(x) selectedMeta %in% x)
     
+    selectedMeta[selectedMeta == TRUE] <- "#000000" 
+    selectedMeta[selectedMeta == FALSE] <- "#00000000" 
+    colnames(selectedMeta) <- uniq
     
-    cols <- IDBacApp::colorBlindPalette()
-    
-    
-    colsd <- cols[factor(selectedMeta[,2])]
-    
-    
-    
-    IDBacApp::colored_dots(colsd,
+    IDBacApp::colored_dots(selectedMeta,
                            trimdLabsDend,
                            horiz = T,
+                           #rowLabels = uniq,
                            sort_by_labels_order = FALSE)
     
-  } 
+  
 }
 
 
