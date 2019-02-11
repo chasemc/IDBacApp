24d634b21f93a8a85ef34975d80baa54e1dc709a
Chase Clark <cclark42@uic.edu>
2019-02-10 18:44:24 -0600

    Update dendInject.R

--- R/dendInject.R
+++ R/dendInject.R
@@ -1,23 +1,9 @@
 selectInjections_UI <- function(id){
   ns <- NS(id)
-  shiny::absolutePanel(
-   # class = "dendMod_WellPanel",
-    bottom = "50%",
-    right =  "0%",
-    width = "20%",
-    fixed = T,
-    draggable = TRUE,
-    style = "z-index:1002;",
-    shiny::wellPanel(class = "dendDots_WellPanel",
-                     IDBacApp::databaseSelector_UI(ns("databaseSelector")),   
-                     IDBacApp::sampleChooser_UI(ns("chooseNewDBSamples"))
-                     
-                     
-                     
-
-    )
-    )
-                     
+  tagList(
+  actionButton(ns("openInject"), "Inject Samples"),
+  uiOutput(ns("injectSelection"))
+  )
                      
 }
   
@@ -28,6 +14,55 @@ selectInjections_UI <- function(id){
                                       session,
                                       sqlDirectory,
                                       availableExperiments){
+    
+    
+    
+    
+    observeEvent(input$closeInject, {
+      output$injectSelection <- renderUI({
+        # Intentionally Blank
+      })
+      
+      
+    })  
+    
+    observeEvent(input$openInject, ignoreInit = T ,ignoreNULL = T, {
+      ns <- session$ns
+      
+      output$injectSelection <- renderUI(
+        shiny::absolutePanel(
+          # class = "dendMod_WellPanel",
+          bottom = "5%",
+          right =  "5%",
+          width = "50%",
+          fixed = T,
+          draggable = TRUE,
+          style = "z-index:1002;",
+          shiny::wellPanel(class = "dendDots_WellPanel",
+                           IDBacApp::databaseSelector_UI(ns("databaseSelector")),   
+                           IDBacApp::sampleChooser_UI(ns("chooseNewDBSamples")),
+                           shiny::actionButton(ns("closeInject"),
+                                               "Close")
+                           
+                           
+                           
+                           
+          )
+        )
+        
+      )  
+    })
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
     
     
