81a2b3e7eb4fee961bb064ed1ac9559506faee4d
Chase Clark <cclark42@uic.edu>
2019-02-24 13:31:31 -0600

    tests

--- tests/testthat/test-readacqufile.R
+++ tests/testthat/test-readacqufile.R
@@ -1,8 +1,10 @@
 context("test-readacqufile")
 
-set.seed(42)
+
 a <- system.file("extdata", file.path("RawData","0_C1","1","1SLin","fid")  , package = "IDBacApp")
 a <- IDBacApp::readAcquFile(a)
+a <- a[names(a) != "file"]
+
 test_that("Parse Bruker acqu", {
-  expect_known_hash(a, "347cb5d5c7")
+  expect_known_hash(a, "2a07967348")
 })
