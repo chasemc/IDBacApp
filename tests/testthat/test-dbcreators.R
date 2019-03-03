40b7d036423753e65c5ec8b06b6bdc50e9ac8084
Chase Clark <cclark42@uic.edu>
2019-03-02 23:42:50 -0600

    Update test-dbcreators.R

--- tests/testthat/test-dbcreators.R
+++ tests/testthat/test-dbcreators.R
@@ -52,44 +52,44 @@ test_that("metaData database table creation", {
 
 
 # Create XML --------------------------------------------------------------
-
-test_that("no metaData", {
-  expect_false(DBI::dbExistsTable(con, "XML"))
-})
-
-path <- system.file(file.path("extdata", "mzML","mzml.mzML"), package = 'IDBacApp')
-
-mzcon <- mzR::openMSfile(path)
-
-out <- IDBacApp::createXMLSQL(rawDataFilePath = path,
-                       userDBCon = con,
-                       mzML_con = mzcon)
-
-IDBacApp::createXMLSQL(rawDataFilePath = path,
-                       userDBCon = con,
-                       mzML_con = mzcon)
-
-a <-  DBI::dbGetQuery(con, "SELECT * FROM XML")
-b <- a$XML[[1]]
-b <- a$XML[[1]]
-
-test_that("XML database table creation", {
-  expect_true(DBI::dbExistsTable(con, "XML"))
-  expect_identical(nrow(a), 1L)
-  expect_identical(ncol(a), 8L)
-  expect_identical(class(b), "raw")
-  expect_identical(out$mzMLHash, "3b3a0e4975b86523")
-  
-})
-
-
-
-
-
-# Create Individual -------------------------------------------------------
-
-
-
-
+# 
+# test_that("no metaData", {
+#   expect_false(DBI::dbExistsTable(con, "XML"))
+# })
+# 
+# path <- system.file(file.path("extdata", "mzML","mzml.mzML"), package = 'IDBacApp')
+# 
+# mzcon <- mzR::openMSfile(path)
+# 
+# out <- IDBacApp::createXMLSQL(rawDataFilePath = path,
+#                        userDBCon = con,
+#                        mzML_con = mzcon)
+# 
+# IDBacApp::createXMLSQL(rawDataFilePath = path,
+#                        userDBCon = con,
+#                        mzML_con = mzcon)
+# 
+# a <-  DBI::dbGetQuery(con, "SELECT * FROM XML")
+# b <- a$XML[[1]]
+# b <- a$XML[[1]]
+# 
+# test_that("XML database table creation", {
+#   expect_true(DBI::dbExistsTable(con, "XML"))
+#   expect_identical(nrow(a), 1L)
+#   expect_identical(ncol(a), 8L)
+#   expect_identical(class(b), "raw")
+#   expect_identical(out$mzMLHash, "3b3a0e4975b86523")
+#   
+# })
+# 
+# 
+# 
+# 
+# 
+# # Create Individual -------------------------------------------------------
+# 
+# 
+# 
+# 
 pool::poolReturn(con)
 pool::poolClose(pool)
