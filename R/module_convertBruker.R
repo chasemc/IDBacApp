cc165dc5a639618e1ad6f2f1acb1b1f3cc973589
chase <cclark42@uic.edu>
2019-05-03 11:47:39 -0500

    ws

--- R/module_convertBruker.R
+++ R/module_convertBruker.R
@@ -73,7 +73,7 @@ convertOneBruker_Server <- function(input,
     if (input$rawFileDirectory > 0) {
       loc <- IDBacApp::choose_dir()
       
-      if(!is.na(loc)){
+      if (!is.na(loc)) {
         return(loc)
       }
     }
@@ -187,6 +187,8 @@ convertOneBruker_Server <- function(input,
                  
                  
                })
+  
+  
   observeEvent(input$saveSampleMap, 
                ignoreInit = TRUE, {  
                  
@@ -302,59 +304,5 @@ convertOneBruker_Server <- function(input,
                  
                })
   
-  
-  
-  
-  
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#' Find which MALDI-target spots have data but an ID wasn't assigned 
-#'
-#' @param spots spot locations that have data 
-#' @param sampleMap 
-#'
-#' @return
-#' @export
-#'
-#' @examples
-findMissingSampleMapIds <- function(spots, 
-                                    sampleMap){
-  
-  if (is.character(spots)) {
-    
-    if (is.data.frame(sampleMap)) {
-      # create sample map
-      aa <- IDBacApp::map384Well()
-      s1 <- base::as.matrix(sampleMap)
-      # Which sample locations have data but weren't assigned an ID?
-      b <- sapply(spots, function(x) s1[which(aa %in% x)])
-      # Return as character vector of spot locations
-      as.character(spots[which(is.na(b))])
-      
-    } else {
-      warning("'findMissingSampleMapIds(sampleMap = )' expected data.frame input \n \n",
-              "'provided input:' \n",
-              sampleMap, 
-              "\n \n")
-    }
-  } else {
-    warning("'findMissingSampleMapIds(sampleMap = )' expected character input \n \n",
-            "'provided input:' \n",
-            spots, 
-            "\n \n")
   }
-}
 
