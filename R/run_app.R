ff8841fd78d208f8b4e10eb32154de2fa19e46ce
Chase Clark <cclark42@uic.edu>
2019-05-07 18:02:36 -0500

    change run_app for electric shine
    hopefully prevents double spawning

--- R/run_app.R
+++ R/run_app.R
@@ -1,9 +1,14 @@
 #' run_app
 #'
+#' @param port The TCP port that the application should listen on
+#'
 #' @return NA
 #' @export
 #'
 
-run_app <- function() {
-  shiny::shinyApp(ui = IDBacApp::app_ui(), server = IDBacApp::app_server)
+run_app <- function(port = 9191) {
+  
+  shiny::shinyApp(ui = IDBacApp::app_ui(),
+                  server = IDBacApp::app_server,
+                  options = list(port = port))
 }
