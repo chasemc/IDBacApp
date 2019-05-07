28ca8241ea9fe3f3c95a250746949a1f59d0b891
Chase Clark <cclark42@uic.edu>
2019-05-07 18:00:08 -0500

    update runapp for electricShine

--- R/run_app.R
+++ R/run_app.R
@@ -4,6 +4,9 @@
 #' @export
 #'
 
-run_app <- function() {
-  shiny::shinyApp(ui = IDBacApp::app_ui(), server = IDBacApp::app_server)
+run_app <- function(port = 9191) {
+  
+  shiny::shinyApp(ui = IDBacApp::app_ui(),
+                  server = IDBacApp::app_server,
+                  options = list(port = 9191))
 }
