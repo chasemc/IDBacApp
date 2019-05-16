ec71de2e481426f4d6d2788e83669bb7d0e56b82
Chase Clark <cclark42@uic.edu>
2019-05-16 15:32:21 -0500

    Skip msconvert tests if not on Windows

--- tests/testthat/test-controlBrukerDisplay.R
+++ tests/testthat/test-controlBrukerDisplay.R
@@ -15,10 +15,8 @@ d <- IDBacApp::controlBrukerDisplay(session, "error", ostest = "sjd")
 
 
 test_that("controlBrukerDisplay works", {
-  expect_known_hash(d, "6f0929edb5")
-  
   skip_on_os(c("mac", "linux", "solaris"))
-  
+  expect_known_hash(d, "6f0929edb5")
   expect_known_hash(a, "fc64e7ede3")
   expect_known_hash(b, "5a13d034da")
 })
