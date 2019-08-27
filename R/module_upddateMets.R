cae02384cedbc3515b39a252b50340edc93687b0
Chase Clark <cclark42@uic.edu>
2019-08-27 16:33:22 -0500

    fixes #131

--- R/module_upddateMets.R
+++ R/module_upddateMets.R
@@ -46,13 +46,13 @@ updateMeta_server <- function(input,
                               session,
                               pool,
                               selectedDB){
-       
+  
   #----
   rhand <- reactiveValues(rtab = data.frame("Strain_ID" = "Placeholder"))
   
   #----
   output$metaTable <- rhandsontable::renderRHandsontable({
-
+    
     
     rhandsontable::rhandsontable(rhand$rtab,
                                  useTypes = FALSE,
@@ -78,7 +78,7 @@ updateMeta_server <- function(input,
                })
   
   
-
+  
   
   observeEvent(input$saven, 
                ignoreInit = TRUE, {
@@ -88,6 +88,8 @@ updateMeta_server <- function(input,
                    title = "Saving...",
                    easyClose = FALSE, 
                    footer = ""))   
+                
+                 #----
                  #make sure not to use the wrong metadata table
                  query <- glue::glue_sql("SELECT  `Strain_ID`
                                           FROM `metaData`",
@@ -96,77 +98,81 @@ updateMeta_server <- function(input,
                  
                  # [-1, because first row is example row
                  userTab <- as.character(rhandsontable::hot_to_r(input$metaTable)[-1, 1])
-                 req(identical(userTab, query))
+                 
+                 tes <- identical(sort(userTab),
+                                  sort(query))
+                 validate(need(tes, "Metadata rhandsontable Strain_IDs didn't match database Strain_IDs"))
+                 #----
                  
                  DBI::dbWriteTable(conn = pool(),
                                    name = "metaData",
                                    value = rhandsontable::hot_to_r(input$metaTable)[-1, ], # remove example row 
                                    overwrite = TRUE)  
-               
+                 
                  removeModal()
                  
                })
   
- 
+  
   
   
   #----
   observeEvent(c(selectedDB$selectExperiment, input$insertNewMetaColumn),{
-                 
-     if (identical(selectedDB$selectExperiment, "None")) {
-       rhand$rtab <- data.frame("Strain_ID" = "Placeholder")
-       } else {
-         
     
-                 if (!is.null(pool())) {
-                   conn <- pool::poolCheckout(pool())
-                   
-                   if (!"metaData" %in% DBI::dbListTables(conn)) {
-                     
-                     warning("It appears the experiment file may be corrupt, please create again.")
-                     rhand$rtab <- data.frame(Strain_ID = "It appears the experiment file may be corrupt, please create the experiment again.")
-                     
-                   } else{
-                     
-                     dbQuery <- glue::glue_sql("SELECT *
+    if (identical(selectedDB$selectExperiment, "None")) {
+      rhand$rtab <- data.frame("Strain_ID" = "Placeholder")
+    } else {
+      
+      
+      if (!is.null(pool())) {
+        conn <- pool::poolCheckout(pool())
+        
+        if (!"metaData" %in% DBI::dbListTables(conn)) {
+          
+          warning("It appears the experiment file may be corrupt, please create again.")
+          rhand$rtab <- data.frame(Strain_ID = "It appears the experiment file may be corrupt, please create the experiment again.")
+          
+        } else{
+          
+          dbQuery <- glue::glue_sql("SELECT *
                                              FROM ({tab*})",
-                                               tab = "metaData",
-                                               .con = conn)
-                     
-                     dbQuery <- DBI::dbGetQuery(conn, dbQuery)
-                     
-                     exampleMetaData <- data.frame(      "Strain_ID"                    = "Example_Strain",
-                                                         "Genbank_Accession"            = "KY858228",
-                                                         "NCBI_TaxID"                   = "446370",
-                                                         "Kingdom"                      = "Bacteria",
-                                                         "Phylum"                       = "Firmicutes",
-                                                         "Class"                        = "Bacilli",
-                                                         "Order"                        = "Bacillales",
-                                                         "Family"                       = "Paenibacillaceae",
-                                                         "Genus"                        = "Paenibacillus",
-                                                         "Species"                      = "telluris",
-                                                         "MALDI_Matrix"                 = "CHCA",
-                                                         "DSM_Agar_Media"               = "1054_Fresh",
-                                                         "Cultivation_Temp_Celsius"     = "27",
-                                                         "Cultivation_Time_Days"        = "10",
-                                                         "Cultivation_Other"            = "",
-                                                         "User"                         = "Chase Clark",
-                                                         "User_ORCID"                   = "0000-0001-6439-9397",
-                                                         "PI_FirstName_LastName"        = "Brian Murphy",
-                                                         "PI_ORCID"                     = "0000-0002-1372-3887",
-                                                         "dna_16S"                      = "TCCTGCCTCAGGACGAACGCTGGCGGCGTGCCTAATACATGCAAGTCGAGCGGAGTTGATGGAGTGCTTGCACTCCTGATGCTTAGCGGCGGACGGGTGAGTAACACGTAGGTAACCTGCCCGTAAGACTGGGATAACATTCGGAAACGAATGCTAATACCGGATACACAACTTGGTCGCATGATCGGAGTTGGGAAAGACGGAGTAATCTGTCACTTACGGATGGACCTGCGGCGCATTAGCTAGTTGGTGAGGTAACGGCTCACCAAGGCGACGATGCGTAGCCGACCTGAGAGGGTGATCGGCCACACTGGGACTGAGACACGGCCCAGACTCCTACGGGAGGCAGCAGTAGGGAATCTTCCGCAATGGACGAAAGTCTGACGGAGCAACGCCGCGTGAGTGATGAAGGTTTTCGGATCGTAAAGCTCTGTTGCCAGGGAAGAACGCTAAGGAGAGTAACTGCTCCTTAGGTGACGGTACCTGAGAAGAAAGCCCCGGCTAACTACGTGCCAGCAGCCGCGGTAATACGTAGGGGGCAAGCGTTGTCCGGAATTATTGGGCGTAAAGCGCGCGCAGGCGGCCTTGTAAGTCTGTTGTTTCAGGCACAAGCTCAACTTGTGTTCGCAATGGAAACTGCAAAGCTTGAGTGCAGAAGAGGAAAGTGGAATTCCACGTGTAGCGGTGAAATGCGTAGAGATGTGGAGGAACACCAGTGGCGAAGGCGACTTTCTGGGCTGTAACTGACGCTGAGGCGCGAAAGCGTGGGGAGCAAACAGGATTAGATACCCTGGTAGTCCACGCCGTAAACGATGAATGCTAGGTGTTAGGGGTTTCGATACCCTTGGTGCCGAAGTTAACACATTAAGCATTCCGCCTGGGGAGTACGGTCGCAAGACTGAAACTCAAAGGAATTGACGGGGACCCGCACAAGCAGTGGAGTATGTGGTTTAATTCGAAGCAACGCGAAGAACCTTACCAGGTCTTGACATCCCTCTGAATCTGCTAGAGATAGCGGCGGCCTTCGGGACAGAGGAGACAGGTGGTGCATGGTTGTCGTCAGCTCGTGTCGTGAGATGTTGGGTTAAGTCCCGCAACGAGCGCAACCCTTGATCTTAGTTGCCAGCAGGTKAAGCTGGGCACTCTAGGATGACTGCCGGTGACAAACCGGAGGAAGGTGGGGATGACGTCAAATCATCATGCCCCTTATGACCTGGGCTACACACGTACTACAATGGCCGATACAACGGGAAGCGAAACCGCGAGGTGGAGCCAATCCTATCAAAGTCGGTCTCAGTTCGGATTGCAGGCTGCAACTCGCCTGCATGAAGTCGGAATTGCTAGTAATCGCGGATCAGCATGCCGCGGTGAATACGTTCCCGGGTCTTGTACACACCGCCCGTCACACCACGAGAGTTTACAACACCCGAAGCCGGTGGGGTAACCGCAAGGAGCCAGCCGTCGAAGGTGGGGTAGATGATTGGGGTGAAGTCGTAAC"
-                     )
-                     
-                     rhand$rtab <- merge(exampleMetaData,
-                                          dbQuery,
-                                          all = TRUE,
-                                          sort = FALSE)
-                     
-                     pool::poolReturn(conn)
-                   }
-                 }
-                 }
-               })
+                                    tab = "metaData",
+                                    .con = conn)
+          
+          dbQuery <- DBI::dbGetQuery(conn, dbQuery)
+          
+          exampleMetaData <- data.frame(      "Strain_ID"                    = "Example_Strain",
+                                              "Genbank_Accession"            = "KY858228",
+                                              "NCBI_TaxID"                   = "446370",
+                                              "Kingdom"                      = "Bacteria",
+                                              "Phylum"                       = "Firmicutes",
+                                              "Class"                        = "Bacilli",
+                                              "Order"                        = "Bacillales",
+                                              "Family"                       = "Paenibacillaceae",
+                                              "Genus"                        = "Paenibacillus",
+                                              "Species"                      = "telluris",
+                                              "MALDI_Matrix"                 = "CHCA",
+                                              "DSM_Agar_Media"               = "1054_Fresh",
+                                              "Cultivation_Temp_Celsius"     = "27",
+                                              "Cultivation_Time_Days"        = "10",
+                                              "Cultivation_Other"            = "",
+                                              "User"                         = "Chase Clark",
+                                              "User_ORCID"                   = "0000-0001-6439-9397",
+                                              "PI_FirstName_LastName"        = "Brian Murphy",
+                                              "PI_ORCID"                     = "0000-0002-1372-3887",
+                                              "dna_16S"                      = "TCCTGCCTCAGGACGAACGCTGGCGGCGTGCCTAATACATGCAAGTCGAGCGGAGTTGATGGAGTGCTTGCACTCCTGATGCTTAGCGGCGGACGGGTGAGTAACACGTAGGTAACCTGCCCGTAAGACTGGGATAACATTCGGAAACGAATGCTAATACCGGATACACAACTTGGTCGCATGATCGGAGTTGGGAAAGACGGAGTAATCTGTCACTTACGGATGGACCTGCGGCGCATTAGCTAGTTGGTGAGGTAACGGCTCACCAAGGCGACGATGCGTAGCCGACCTGAGAGGGTGATCGGCCACACTGGGACTGAGACACGGCCCAGACTCCTACGGGAGGCAGCAGTAGGGAATCTTCCGCAATGGACGAAAGTCTGACGGAGCAACGCCGCGTGAGTGATGAAGGTTTTCGGATCGTAAAGCTCTGTTGCCAGGGAAGAACGCTAAGGAGAGTAACTGCTCCTTAGGTGACGGTACCTGAGAAGAAAGCCCCGGCTAACTACGTGCCAGCAGCCGCGGTAATACGTAGGGGGCAAGCGTTGTCCGGAATTATTGGGCGTAAAGCGCGCGCAGGCGGCCTTGTAAGTCTGTTGTTTCAGGCACAAGCTCAACTTGTGTTCGCAATGGAAACTGCAAAGCTTGAGTGCAGAAGAGGAAAGTGGAATTCCACGTGTAGCGGTGAAATGCGTAGAGATGTGGAGGAACACCAGTGGCGAAGGCGACTTTCTGGGCTGTAACTGACGCTGAGGCGCGAAAGCGTGGGGAGCAAACAGGATTAGATACCCTGGTAGTCCACGCCGTAAACGATGAATGCTAGGTGTTAGGGGTTTCGATACCCTTGGTGCCGAAGTTAACACATTAAGCATTCCGCCTGGGGAGTACGGTCGCAAGACTGAAACTCAAAGGAATTGACGGGGACCCGCACAAGCAGTGGAGTATGTGGTTTAATTCGAAGCAACGCGAAGAACCTTACCAGGTCTTGACATCCCTCTGAATCTGCTAGAGATAGCGGCGGCCTTCGGGACAGAGGAGACAGGTGGTGCATGGTTGTCGTCAGCTCGTGTCGTGAGATGTTGGGTTAAGTCCCGCAACGAGCGCAACCCTTGATCTTAGTTGCCAGCAGGTKAAGCTGGGCACTCTAGGATGACTGCCGGTGACAAACCGGAGGAAGGTGGGGATGACGTCAAATCATCATGCCCCTTATGACCTGGGCTACACACGTACTACAATGGCCGATACAACGGGAAGCGAAACCGCGAGGTGGAGCCAATCCTATCAAAGTCGGTCTCAGTTCGGATTGCAGGCTGCAACTCGCCTGCATGAAGTCGGAATTGCTAGTAATCGCGGATCAGCATGCCGCGGTGAATACGTTCCCGGGTCTTGTACACACCGCCCGTCACACCACGAGAGTTTACAACACCCGAAGCCGGTGGGGTAACCGCAAGGAGCCAGCCGTCGAAGGTGGGGTAGATGATTGGGGTGAAGTCGTAAC"
+          )
+          
+          rhand$rtab <- merge(exampleMetaData,
+                              dbQuery,
+                              all = TRUE,
+                              sort = TRUE)
+          
+          pool::poolReturn(conn)
+        }
+      }
+    }
+  })
   
 }
 
