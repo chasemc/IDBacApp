fcae376ae9956273760a03a5591fa274fa980061
chase <cclark42@uic.edu>
2020-01-03 11:03:25 -0600

    devtools::document()

--- R/sql_update_version.R
+++ R/sql_update_version.R
@@ -1,37 +1,37 @@
-
-
-old_db_con <- IDBacApp::createPool("hiya",
-                                   "C:/Users/CMC/IDBac_experiments")[[1]]
-
-new_db_con <- IDBacApp::createNewSQLITEdb("hiya_new",
-                                          "C:/Users/CMC/IDBac_experiments")[[1]]
-
-
-update_database <- function(old_db_con,
-                            new_db_con){
-  
-  
-  "spectra"
-  "locale"
-  "mass_index"
-  "metadata"
-  "version"
-  "xml"
-  
-  
-  
-  
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
+# 
+# 
+# old_db_con <- IDBacApp::createPool("hiya",
+#                                    "C:/Users/CMC/IDBac_experiments")[[1]]
+# 
+# new_db_con <- IDBacApp::createNewSQLITEdb("hiya_new",
+#                                           "C:/Users/CMC/IDBac_experiments")[[1]]
+# 
+# 
+# update_database <- function(old_db_con,
+#                             new_db_con){
+#   
+#   
+#   "spectra"
+#   "locale"
+#   "mass_index"
+#   "metadata"
+#   "version"
+#   "xml"
+#   
+#   
+#   
+#   
+# }
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
